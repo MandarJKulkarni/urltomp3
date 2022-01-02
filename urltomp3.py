cead710ea9b05b3aa3bde33844f0b658d3d4e6c3
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2022-01-02 23:46:59 +0530

    add play method

--- urltomp3.py
+++ urltomp3.py
@@ -1,6 +1,10 @@
 import os
+
+import requests
 from gtts import gTTS
 from newspaper import Article
+from newspaper import fulltext
+from pygame import mixer
 
 
 def get_text_from_url(url):
@@ -16,6 +20,9 @@ def get_text_from_url(url):
 def mp3_from_url(input_url):
     url_text = get_text_from_url(input_url)
 
+    html = requests.get(input_url).text
+    text = fulltext(html)
+
     if url_text:
         mp3file = './/article.mp3'
         if os.path.exists(mp3file):
@@ -23,3 +30,9 @@ def mp3_from_url(input_url):
         gTTS(url_text).save(mp3file)
         return input_url
     return "Unable to extract the data from the page"
+
+
+def play(mp3file):
+    mixer.init()
+    mixer.music.load(mp3file)
+    mixer.music.play()
